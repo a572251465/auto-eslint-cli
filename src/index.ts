fb29f2511830609e92751651cea45c8e430b3c40
李浩浩 <lihaohao@uino.com>
2021-08-28 17:50:11 +0800

    feat: 实现进度条的功能

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,35 @@ const chalk = require("chalk");
 const { program } = require("commander");
 const path = require("path");
 const fs = require("fs");
+const ProgressBar = require("progress");
 
 const packageError = "Error: file package.json does not exist, init fail";
 const cwd = process.cwd();
 
-// eslint-disable-next-line consistent-return
-const promptHandle = async () => {
-  require("./cli-prompt/index.ts")();
-};
+// eslint-disable-next-line consistent-return,no-async-promise-executor
+const promptHandle = () =>
+  // eslint-disable-next-line no-async-promise-executor
+  new Promise(async (resolve) => {
+    await new Promise((resolve1) => {
+      console.log(chalk.cyan(" File analysis in progress ..."));
+      const timer = setTimeout(() => {
+        resolve1(true);
+        clearTimeout(timer);
+      }, 1000);
+    });
+    const bar = new ProgressBar(":bar :current/:total%", { total: 100 });
+    const timer = setInterval(() => {
+      bar.tick();
+      if (bar.complete) {
+        clearInterval(timer);
+        require("./cli-prompt/index.ts")();
+        resolve(true);
+      }
+    }, 50);
+  });
 
 // eslint-disable-next-line consistent-return
-const run = () => {
+const run = async () => {
   const packagePath = path.resolve(cwd, "package.json");
   let stat = null;
   try {
@@ -33,8 +51,8 @@ program
   .option("-n --name", "please input config file name")
   .command("init")
   .description("eslint config init handle")
-  .action(() => {
-    run();
+  .action(async () => {
+    await run();
   });
 
 program.parse(process.argv);
